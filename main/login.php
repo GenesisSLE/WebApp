90522251bb04b913555bcd560a0172927512655f
Fabri <31490939+SupahFox@users.noreply.github.com>
2022-09-09 10:08:01 -0300

    modified:   main/conexion.php 	modified:   main/login.php

--- main/login.php
+++ main/login.php
@@ -5,7 +5,7 @@ $usuarios = $mysqli->query("SELECT Nombre, TipoUsuario FROM usuarios WHERE Nombr
 
 if($usuarios->num_rows > 1):
 	$datos=$usuarios->fetch_assoc();
-	echo json_encode(array('error' => false , 'tipo' =>$datos['TipoUsuario'] ));
+	echo json_encode(array('error' => false , 'tipo' => $datos['TipoUsuario'] ));
 else:
 	echo json_encode(array('error' => true));
 endif;
