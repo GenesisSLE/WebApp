d7e57eaa40dd03a3a6ee4e3efaff8589e6f11040
Fabri <31490939+SupahFox@users.noreply.github.com>
2022-09-08 21:13:43 -0300

    new file:   main/Admin/cfg/adscripto/alta.php 	new file:   main/Admin/cfg/adscripto/baja.php 	new file:   main/Admin/cfg/adscripto/css/style.css 	new file:   main/Admin/cfg/adscripto/img/Ejem.png 	new file:   main/Admin/cfg/adscripto/mod.php 	new file:   main/Admin/cfg/docente/alta.php 	new file:   main/Admin/cfg/docente/baja.php 	new file:   main/Admin/cfg/docente/css/style.css 	new file:   main/Admin/cfg/docente/img/Ejem.png 	new file:   main/Admin/cfg/docente/mod.php 	new file:   main/Admin/cfg/inspector/alta.php 	new file:   main/Admin/cfg/inspector/baja.php 	new file:   main/Admin/cfg/inspector/css/style.css 	new file:   main/Admin/cfg/inspector/img/Ejem.png	new file:   main/Admin/cfg/inspector/mod.php
    	new file:   main/Admin/cfg/adscripto/alta.php
    	new file:   main/Admin/cfg/adscripto/baja.php
    	new file:   main/Admin/cfg/adscripto/css/style.css
    	new file:   main/Admin/cfg/adscripto/img/Ejem.png
    	new file:   main/Admin/cfg/adscripto/mod.php
    	new file:   main/Admin/cfg/docente/alta.php
    	new file:   main/Admin/cfg/docente/baja.php
    	new file:   main/Admin/cfg/docente/css/style.css
    	new file:   main/Admin/cfg/docente/img/Ejem.png
    	new file:   main/Admin/cfg/docente/mod.php
    	new file:   main/Admin/cfg/inspector/alta.php
    	new file:   main/Admin/cfg/inspector/baja.php
    	new file:   main/Admin/cfg/inspector/css/style.css
    	new file:   main/Admin/cfg/inspector/img/Ejem.png
    	new file:   main/Admin/cfg/inspector/mod.php

---
+++ main/Admin/cfg/inspector/baja.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+    <title>MENU ADMIN</title>
+    <link rel="stylesheet" type="text/css" href="../../../css/styleHeader.css">
+    <link rel="stylesheet" type="text/css" href="../../css/styleMenuAdmin.css">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@500&display=swap" rel="stylesheet">
+
+    <meta charset="UTF-8">
+</head>
+
+<body>
+
+<div class="header">
+<div class="notificacion"><img  src="../../../../img/notificacion.png"></div>
+<div class="usuario">
+    <h2>ADMIN</h2>
+    <div class = "icon">
+        <img src = "../../../../img/user.png" width = "30px" height = "30px">
+    </div>
+</div>
+</div>
+
+</body>
+
+</html>
