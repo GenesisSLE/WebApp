87b0dbd0919bedd59a1590a983f2b1059763d7a6
SantiagoSoto5555 <92376120+SantiagoSoto5555@users.noreply.github.com>
2022-09-12 02:56:43 -0300

    Aa

---
+++ main/Admin/cfg/Admin/baja.php
@@ -0,0 +1,50 @@
+<?php
+ session_start();
+ $elusu = $_SESSION['usu'];
+ var_dump($_SESSION['usu']);
+ if($elusu == null || $elusu == '') {
+   header("location:../../ErrorSession.php");
+    die();
+ }
+?>
+<!DOCTYPE html>
+
+<html>
+<head>
+    <title>MENU ADMIN</title>
+    <link rel="stylesheet" type="text/css" href="../../../css/styleHeader.css">
+    <link rel="stylesheet" type="text/css" href="../../css/styleMenuAdmin.css">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@500&display=swap" rel="stylesheet">
+
+    <meta charset="UTF-8">
+</head>
+
+<body>
+
+<div class="header">
+<div class="notificacion"><img  src="../../../../img/notificacion.png"></div>
+<div class="usuario">
+    <h2>ADMIN</h2>
+    <div class = "icon">
+        <img src = "../../../../img/user.png" width = "30px" height = "30px">
+    </div>
+</div>
+</div>
+
+<div class = "logo">
+<img src = "../../../../img/logo1.png" width = "200px" height = "200px">
+</div>
+
+<div class = "formulario">
+        <form id="formaltausr" method = "post">
+            <input class="curso" type="text" name="cursoA" placeholder="Curso"><br>
+            <input class="grupo" type="text" name="grupoA" placeholder="Grupo">
+        </form>
+</div>
+
+</body>
+
+</html>
