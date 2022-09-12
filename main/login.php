fd7ae20e951bf22f3072ef3068ef06c6cc1253fd
Fabri <31490939+SupahFox@users.noreply.github.com>
2022-09-12 20:27:14 -0300

    tu papa hace crucigramas 2

--- main/login.php
+++ main/login.php
@@ -1,9 +1,9 @@
 <?php
 session_start();
 require 'conexion.php';
-sleep(4);
+sleep(3);
+$_SESSION['usu'] = $_POST['usuariolg'];
 $usuarios = $mysqli->query("SELECT Nombre, TipoUsuario FROM usuarios WHERE Nombre='".$_POST['usuariolg']."' AND pass ='".$_POST['passlg']."'");
-
 if($usuarios->num_rows > 1):
 	$datos=$usuarios->fetch_assoc();
 	echo json_encode(array('error' => false , 'tipo' => $datos['TipoUsuario'] ));
