06670b454e454d17ad954b7129307b3a5b8d4fec
SantiagoSoto5555 <92376120+SantiagoSoto5555@users.noreply.github.com>
2022-09-29 19:23:04 -0300

    s

---
+++ main/Docente/ListAlumnoDoc.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Menú lateral responsive - MagtimusPro</title>
+
+    <link rel="stylesheet" href="css/estilos.css">
+
+    <script src="https://kit.fontawesome.com/41bcea2ae3.js" crossorigin="anonymous"></script>
+</head>
+<body id="body">
+    
+    <header>
+        <div class="icon__menu">
+            <i class="fas fa-bars" id="btn_open"></i>
+        </div>
+       <h1 class="titulo">Listado</h1>
+    </header>
+
+    <div class="menu__side" id="menu_side">
+
+        <div class="name__page">
+             <img class ="loguitos"src="img/logo2.png">
+            <h4>Listype</h4>
+        </div>
+
+        <div class="options__menu">	
+
+            <a href="#" class="selected">
+                <div class="option">
+                    <i class="fas fa-home" title="Inicio"></i>
+                    <h4>Inicio</h4>
+                </div>
+            </a>
+
+            <a href="#">
+                <div class="option">
+                    <i class="far fa-file" title="Portafolio"></i>
+                    <h4>Portafolio</h4>
+                </div>
+            </a>
+            
+            <a href="#">
+                <div class="option">
+                    <i class="fas fa-video" title="Cursos"></i>
+                    <h4>Cursos</h4>
+                </div>
+            </a>
+
+            <a href="#">
+                <div class="option">
+                    <i class="far fa-sticky-note" title="Blog"></i>
+                    <h4>Blog</h4>
+                </div>
+            </a>
+
+
+            <a href="#">
+                <div class="option">
+                    <i class="far fa-address-card" title="Nosotros"></i>
+                    <h4>Nosotros</h4>
+                </div>
+            </a>
+
+			<a href="#">
+                <div class="option">
+                
+           <a href="../../index.php"><img alt="CerrarSesion"width = "33px" height = "33px"src="../../img/cerrar-sesion.png"></a>
+
+                </div>
+            </a>
+        </div>
+
+    </div>
+
+    <main>
+        <div class="tarAlumno"
+    </main>
+
+    <script src="js/script.js"></script>
+</body>
+</html>
