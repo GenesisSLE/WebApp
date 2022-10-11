83180c80c1b36dceaecf5fe9d0363e01ab939743
Fabri <31490939+SupahFox@users.noreply.github.com>
2022-10-11 13:22:57 -0300

    modified:   main/Docente/css/style.css modified:   main/Docente/form.php

--- main/Docente/form.php
+++ main/Docente/form.php
@@ -12,11 +12,27 @@
 </head>
 <body id="body">
 
-<div class = "cntr">
-                <div class="previewcntr">
+<div class = "cntrform">
+                <div class="previewcntrform">
                     <div class = "tipos">
                         <h4 class="tipo">Tipo</h4><h4 class="fecha">Fecha</h4><h4 class="nota">Nota</h4>
                     </div>
+                </div>
+
+                <div class="formulario">
+                    <form method = "post">
+                        <input class="formtipo" type="radio" name="tipo" required placeholder="Tipo">
+                        <input class="formtipo" type="radio" name="tipo1" required placeholder="Tipo">
+                        <input class="formnota" type="number" name="nota" min="1" max="12" required placeholder="Nota">
+
+                        <label>
+    <input type="checkbox" class="radio" value="1" name="fooby[2][]" />Tiger</label>
+  <label>
+    <input type="checkbox" class="radio" value="1" name="fooby[2][]" />Sloth</label>
+  <label>
+    <input type="checkbox" class="radio" value="1" name="fooby[2][]" />Cheetah</label>
+</div>
+                    </form>
                 </div>
 </div>
 
