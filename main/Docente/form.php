2cd7c1d145053f557c129016b6f1dccdcfccbe56
Fabri <31490939+SupahFox@users.noreply.github.com>
2022-10-11 12:48:55 -0300

    modified:   main/Docente/css/style.css new file:   main/Docente/form.php

---
+++ main/Docente/form.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Menú</title>
+
+    <link rel="stylesheet" href="css/style.css">
+
+    <script src="https://kit.fontawesome.com/41bcea2ae3.js" crossorigin="anonymous"></script>
+</head>
+<body id="body">
+
+<div class = "cntr">
+                <div class="previewcntr">
+                    <div class = "tipos">
+                        <h4 class="tipo">Tipo</h4><h4 class="fecha">Fecha</h4><h4 class="nota">Nota</h4>
+                    </div>
+                </div>
+</div>
+
+</body>
+</html>
