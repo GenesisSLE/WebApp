c51c403a699bc2d0def0524ce83aa9d7b42b00ff
Fabri <31490939+SupahFox@users.noreply.github.com>
2022-11-25 08:28:32 -0300

    Update conexion.php

--- main/conexion.php
+++ main/conexion.php
@@ -1,5 +1,5 @@
 <?php
-$mysqli = new mysqli('localhost','genesis','genesis','genesis_sle');
+$mysqli = new mysqli('localhost', 'genesis', 'genesis', 'genesis_sle');
 if($mysqli->connect_errno):
     echo "Error al conectarse con base de datos".$mysqli->connect_error;
 endif;
